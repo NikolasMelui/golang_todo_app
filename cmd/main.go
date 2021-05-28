e7e668623bcffab9e194e92d814321012e63ce5a
nikolasmelui <slak@samaradom.ru>
2021-05-28 23:27:03 +0300

    Add handlers to the main.go file

--- cmd/main.go
+++ cmd/main.go
@@ -4,11 +4,14 @@ import (
 	"log"
 
 	todo "github.com/nikolasmelui/golang_todo_app"
+	"github.com/nikolasmelui/golang_todo_app/pkg/handler"
 )
 
 func main() {
+	handlers := new(handler.Handler)
+
 	srv := new(todo.Server)
-	if err := srv.Run("8000"); err != nil {
+	if err := srv.Run("8000", handlers.InitRoutes()); err != nil {
 		log.Fatalf("error occured while running http server: %s", err.Error())
 	}
 }
